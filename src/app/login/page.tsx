2825adb4bca8e289dbd41d7d57bd00127d8fe238
CollinsMunene-Developer <collinsentrepreneur@gmail.com>
2024-09-28 11:01:23 +0300

    "Added state management for user credentials, updated login function, and added password input field and register link to login page"

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,24 +1,33 @@
-"use client"
-import axios from "axios"
+"use client";
+import axios from "axios";
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const [user, setuser] = useState({
+    email: "",
+    password: "",
+  });
 
-    const onLogin = () => {
-        axios.post("/api/login", {
-            email: "XXXXXXXXXXXXX",
-            password: "XXXX"
-        }).then((res) => {
-            console.log(res)
-        }).catch((err) => {
-            console.log(err)
-        })
-
-    }
-    return (
-
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
-            <h1>Login</h1>
-            <label htmlFor="email">email</label>
+  const onLogin = () => {
+    axios
+      .post("/api/login", {
+        email: "",
+        password: "",
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 ">
+      <h1>Login</h1>
+      <hr />
+      <label htmlFor="email">email</label>
       <input
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none  focus:border-gray-600"
         type="email"
@@ -27,9 +36,22 @@ export default function LoginPage() {
         onChange={(e) => setuser({ ...user, email: e.target.value })}
         placeholder="Input your email"
       />
-            </div>
 
-
-    )
-
+<input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none  focus:border-gray-600"
+        type="password"
+        id="password"
+        value={user.password}
+        onChange={(e) => setuser({ ...user, password: e.target.value })}
+        placeholder="Set your passowrd"
+      />
+      <button
+        className="p-2 border-gray-300  rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        onClick={onLogin}
+      >
+        Login
+      </button>
+      <Link href="/signup">Register</Link>
+    </div>
+  );
 }
